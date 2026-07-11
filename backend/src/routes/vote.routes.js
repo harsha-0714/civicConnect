4ad283754dc954a414a3e66a568092b3d90dc326
Harshavardhan <harshavardhand07@gmail.com>
2026-07-11 22:28:42 +0530

    Delete backend directory

--- backend/src/routes/vote.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/auth.middleware');
-const ctrl = require('../controllers/vote.controller');
-
-router.post('/:issueId', protect, ctrl.upvote);
-router.delete('/:issueId', protect, ctrl.removeVote);
-
-module.exports = router;
